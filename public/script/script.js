c3de3ecec0e0921501be5496efbaa5cfb8cb5eac
Sakura1943 <1436700265@qq.com>
2022-07-15 12:44:37 +0800

    update public/script/script.js

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,33 @@
-alert("ERROR: Hey, bro, KFC Crazy Thursday, V me $50!")
+let Week = new Date().getDay()
+
+switch (Week) {
+  case 0:
+    Week = "Sunday"
+    break;
+  case 1:
+    Week = "Monday"
+    break;
+  case 2:
+    Week = "Tuesday"
+    break;
+  case 3:
+    Week = "Wednesday"
+    break;
+  case 4:
+    Week = "Thursday"
+    break;
+  case 5:
+    Week = "Friday"
+    break;
+  case 6:
+    Week = "Saturday"
+    break;
+  default:
+    Week = undefined
+    break;
+}
+
+alert(`ERROR: Hey, bro, KFC Crazy ${Week}, V me $50!`)
 while(true) {
   let ans = prompt("V me $50?(yes/no):")
   if (ans === "Y" || ans === "y" || ans === "Yes" || ans === "YES" || ans === "yes") {
@@ -6,4 +35,6 @@ while(true) {
   }
 }
 
-throw new Error("Hey, bro, KFC Crazy Thursday, V me $50!")
+
+
+throw new Error(`Hey, bro, KFC Crazy ${Week}, V me $50!`)
